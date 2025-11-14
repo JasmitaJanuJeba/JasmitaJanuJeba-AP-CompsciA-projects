ec4ceb582edcc479ca5b0b5ab4562058cb090517
JasmitaJanuJeba <77160252+JasmitaJanuJeba@users.noreply.github.com>
2025-11-14 23:14:33 +0000

    11/14/25 - Initial Cards Commit (finished Card Class)

--- cards/Card.java
+++ cards/Card.java
@@ -2,5 +2,24 @@ package cards;
 
 public class Card {
 
+//instance variables
+private int suit;
+private int value;
+private static String[] SUITS = {"♦","♥","♣","♠"};
+private static String[] VALUE = {"A","2","3","4","5","6","7","8","9","10","K","Q","J"};
+
+//constructor
+public Card(int suit, int value){
+    this.suit = suit;
+    this.value = value;
+}
+
+//methods 
+public int getValue(){
+    return value;
+}
+public String toString(){
+    return " "+SUITS[suit]+VALUE[value];
+}
 
 }
