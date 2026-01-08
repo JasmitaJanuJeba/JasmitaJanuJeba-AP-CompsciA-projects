ad188e225a8fa410cad64f7df0355d03a315413d
JasmitaJanuJeba <77160252+JasmitaJanuJeba@users.noreply.github.com>
2026-01-08 23:06:32 +0000

    did the first 2

--- warmup/Main.java
+++ warmup/Main.java
@@ -1,9 +1,36 @@
 package warmup;
 
 public class Main {
-
+ int sum;
+ int a = 1;
+int b = 2;
     public static void main(String[] args) {
-        // Your code here.
+        int sum = 0;
+        int a = 1;
+        int b = 2;
+
+
+        for (int i = 1; i < 1000; i++) {
+            if (i % 3 == 0 || i % 5 == 0) {
+                sum += i;
+            }
+        }
+
+        System.out.println(sum);
+ 
+        while (b <= 4_000_000) {
+            if (b % 2 == 0) {
+                sum += b;
+            }
+
+            int next = a + b;
+            a = b;
+            b = next;
+        }
+
+        System.out.println(sum);
 
     }
 }
+
+
